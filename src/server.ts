b9bd5e972716d8369044a255b92d601335409f60
Tabiul Mahmood <tabiul.mahmood@siteminder.com>
2019-09-14 15:28:14 +1000

    improve formatting

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import * as http from "http";
 import * as fs from "fs"
 import args from "args"
 import * as toml from "toml"
+import chalk from "chalk"
+import ansiEscapes from "ansi-escapes"
 
 args.option("port", "port to run", 9080)
 args.option("file", "path to the url matcher toml file", "")
@@ -19,25 +21,31 @@ interface Rule {
 }
 
 const flags = args.parse(process.argv)
+const info = chalk.green
+const error = chalk.red.bold
+const warn = chalk.yellow
+const nestedNextLine =  ansiEscapes.cursorNextLine + ansiEscapes.cursorForward(2)
 
 let rules: Rule = {}
 
 if (flags.file) {
-  console.log(`reading rules file ${flags.file}`)
+  console.log(info(`reading rules file ${flags.file}`))
   rules = toml.parse(fs.readFileSync(flags.file, 'utf8'))
 }
 
 let server = http.createServer((request, response) => {
 
   let path = request.url as string
-  console.log('url', path)
-  console.log('method', request.method)
+  console.log(ansiEscapes.cursorNextLine)
+  console.log(chalk.blue.bold("request:"))
+  console.log(info(nestedNextLine + 'url: %s'), path)
+  console.log(info(nestedNextLine + 'method: %s'), request.method)
   let requestData = ''
   request.on('data', (data) => {
     requestData += data
   })
   request.on('end', () => {
-    console.log('body', requestData)
+    console.log(info(nestedNextLine + 'body: %s'), requestData)
     processRequest(path, response)
   })
 });
@@ -48,10 +56,11 @@ process.on('SIGINT', () => {
 
 let processRequest = function (requestUrl: String, response: http.ServerResponse) {
   if (rules) {
+    console.log(ansiEscapes.cursorNextLine)
     for (let path in rules) {
-      console.log(`check rule ${path}`)
+      console.log(info(`check rule ${path}`))
       if (requestUrl.search(path) != -1) {
-        console.log(`found matching rule ${path}`)
+        console.log(nestedNextLine + info(`found matching rule ${path}`))
         if (rules[path].headers) {
           for (let header in rules[path].headers) {
             response.setHeader(header, rules[path].headers![header])
@@ -60,22 +69,23 @@ let processRequest = function (requestUrl: String, response: http.ServerResponse
         if (rules[path].responseCode) {
           response.writeHead(rules[path].responseCode)
         } else {
-          console.log('no response code, defaulting to Http 200')
+          console.log(nestedNextLine + warn('no response code, defaulting to Http 200'))
           response.writeHead(200)
         }
         if (rules[path].body) response.write(rules[path].body)
         break
       } else {
-        console.log("rule was not matched")
+        console.log(nestedNextLine + warn("rule was not matched"))
       }
+      console.log(ansiEscapes.cursorNextLine)
     }
     response.end()
   } else {
-    console.log('no rules defined, responding with Http 200')
+    console.log(warn('no rules defined, responding with Http 200'))
     response.writeHead(200)
     response.end()
   }
 }
 
-console.log('starting server on port', flags.port)
+console.log(chalk.green('starting server on port: %d'), flags.port)
 server.listen(flags.port);
